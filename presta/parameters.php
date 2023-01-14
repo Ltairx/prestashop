b85a05cb0f7f5628232f2ae7d418e4d01907d526
Sugar <69794648+Ltairx@users.noreply.github.com>
2023-01-14 18:27:25 +0100

    Add files via upload

---
+++ presta/parameters.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+ 'parameters' =>
+array(
+'database_host' => 'db',
+'database_port' => '3306',
+'database_name' => 'prestashop',
+'database_user' => 'prestashopuser',
+'database_password' => 'test',
+'database_prefix' => 'ps_',
+'database_engine' => 'InnoDB',
+'ps_caching' => 'CacheMemcache',
+'ps_cache_enable' => false,
+'ps_creation_date' => '2023-01-01',
+),
+);
